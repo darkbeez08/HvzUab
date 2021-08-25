b2585ed66817d929cd7df1b0b3f7df52bb01deed
darkb <darkb@DESKTOP-6OQ3J3V>
2021-08-25 02:55:11 +0200

    HALAL

---
+++ HvzUab/HvzUab/HvzUab/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace HvzUab
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("Resources/HALAL/ 1280px - Kaaba_Masjid_Al - Haram_Mecca_Saudi_Arabia_ - _panoramio");
+            //await Launcher.OpenAsync("https://www.google.com/search?q=meca&client=opera-gx&hs=aWk&sxsrf=ALeKk03o17PQrSeAlhZpRP_ygeZUB6EC3g:1629851372030&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiYzsPA9cryAhVxzoUKHWFPBpEQ_AUoAXoECAEQAw&biw=1319&bih=658#imgrc=w06KMYrQG0PZ7M");
+        }
+    }
+}
